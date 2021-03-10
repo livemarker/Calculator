3b5315be959fdbd74a850395af132a00c66f9533
Elovenko Nikita <livemarker@users.noreply.github.com>
2021-03-10 12:11:32 +0300

    fix division

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -11,50 +11,78 @@ public class Calculator {
 
 
     public static void main(String[] args) throws IOException {
-      Calculator calculator=new Calculator();
-      calculator.initialize();
+        Calculator calculator = new Calculator();
+        calculator.initialize();
         System.out.println("result: ");
-        System.out.println(calculator.calculate(num1,num2,character));
+        System.out.println(calculator.calculate(num1, num2, character));
 
     }
-    private  void initialize() throws IOException {
-        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+
+    private void initialize() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("put in first num");
-        num1=Double.parseDouble(reader.readLine());
+        num1 = Double.parseDouble(reader.readLine());
         System.out.println("put in character");
-        character= reader.readLine();
+        character = reader.readLine();
         System.out.println("put in second num");
-        num2=Double.parseDouble(reader.readLine());
+        num2 = Double.parseDouble(reader.readLine());
     }
-    private double sum(double a, double b){
-        return a+b;
+
+    private double sum(double num1, double num2) {
+        return num1 + num2;
     }
-    private double multiplication(double a, double b){
-        return a*b;
+
+    private double multiplication(double num1, double num2) {
+        return num1 * num2;
     }
-    private double division(double a, double b){
-        return a/b;
+
+    private double division(double num1, double num2) {
+        double result=0;
+        try {
+             result= num1 / num2;
+        } catch (ArithmeticException exception) {
+            System.out.println("error division");
+        }
+        return result;
     }
-    private double substraction(double a, double b){
-        return a-b;
+
+    private double substraction(double num1, double num2) {
+        return num1 - num2;
     }
-    private double degree(double a, double b){
-        double result=a;
-        for (int i = 1; i <b ; i++) {
-            result*=a;
+
+    private double degree(double num1, double num2) {
+        double result = num1;
+        for (int i = 1; i < num2; i++) {
+            result *= num1;
         }
         return result;
     }                                                        // можно добавить еще возможности реализации
-    private double calculate(double a, double b, String c){
+
+    private double calculate(double num1, double num2, String character) {
         double result;
-        switch (c){
-            case "/":{result= division(a, b);break;}
-            case "*":{result=multiplication(a, b);break;}
-            case "+":{result=sum(a, b);break;}
-            case "-":{result=substraction(a, b);break;}
-            case "^" :{result=degree(a, b);break;}  // можно добавить еще возможности реализации
+        switch (character) {
+            case "/": {
+                result = division(num1, num2);
+                break;
+            }
+            case "*": {
+                result = multiplication(num1, num2);
+                break;
+            }
+            case "+": {
+                result = sum(num1, num2);
+                break;
+            }
+            case "-": {
+                result = substraction(num1, num2);
+                break;
+            }
+            case "^": {
+                result = degree(num1, num2);
+                break;
+            }  // можно добавить еще возможности реализации
             default:
-                throw new IllegalStateException("Unexpected value: " + c);
+                throw new IllegalStateException("Unexpected value: " + character);
         }
         return result;
     }
